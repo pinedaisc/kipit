ad216b916e761caa8228448bb8c3b96b6ef4180d
pinedaisc <pinedaisc@gmail.com>
2019-05-21 18:02:15 -0500

    algunas funcionalidades (solo pruebas)

--- js/script.js
+++ js/script.js
@@ -2,4 +2,25 @@ console.log("script fue cargado")
 
 $(".btn").click(function(e){
     var a = alert("click on " + $(this).attr('class'));
+    send()
 })
+
+$(".cuerpo").focusout(function(){
+    console.log($(this).find(".text").val());
+    $(this).css("border","none")
+    send()
+})
+
+$(".nota").click(function(){
+    $(this).find(".cuerpo").fadeIn("slow").css("border","1px solid gray")
+    send()
+})
+
+
+
+function send(){
+    console.log("funcion send alcanzada!!!!")
+}
+
+function edit(){}
+function remove(){}
